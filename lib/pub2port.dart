0c543f819cc599477f04c935241b188dee6d9827
Aaron Madlon-Kay <aaron@madlon-kay.com>
2025-07-01 11:15:15 +0900

    Refactor for clarity

--- lib/pub2port.dart
+++ lib/pub2port.dart
@@ -8,31 +8,26 @@ void lockToPort(PubspecLock lockfile, {StringSink? out, StringSink? err}) {
 
   final packages = lockfile.packages.toList(growable: false);
 
-  if (packages.every(
-    (p) => p.iswitcho(hosted: (_) => false, otherwise: () => true),
-  )) {
+  if (packages.every((p) => p.isNotHosted)) {
     err.writeln('No hosted packages found');
     return;
   }
   final maxNameLength = packages
-      .where((p) => p.iswitcho(hosted: (_) => true, otherwise: () => false))
+      .where((p) => p.isHosted)
       .map((e) => e.package().length)
       .reduce((a, b) => a > b ? a : b);
   final maxVersionLength = packages
-      .where((p) => p.iswitcho(hosted: (_) => true, otherwise: () => false))
+      .where((p) => p.isHosted)
       .map((e) => e.version().length)
       .reduce((a, b) => a > b ? a : b);
 
   out.writeln(r'pub.packages \');
   for (final package in packages) {
-    if (!package.iswitcho(hosted: (_) => true, otherwise: () => false)) {
+    if (!package.isHosted) {
       err.writeln('Skipping ${package.package()}: not hosted');
       continue;
     }
-    final hash = package.iswitcho(
-      hosted: (h) => h.sha256,
-      otherwise: () => null,
-    );
+    final hash = package.sha256;
     if (hash == null) {
       err.writeln('Skipping ${package.package()}: missing sha256');
       continue;
@@ -49,4 +44,12 @@ void lockToPort(PubspecLock lockfile, {StringSink? out, StringSink? err}) {
     }
     out.writeln();
   }
+}
+
+extension _PackageDependencyExt on PackageDependency {
+  bool get isHosted => iswitcho(hosted: (_) => true, otherwise: () => false);
+  bool get isNotHosted => !isHosted;
+
+  String? get sha256 =>
+      iswitcho(hosted: (h) => h.sha256, otherwise: () => null);
 }
